66be06e59d7873a208058ca3f76427f964c0a056
pasha_abramchuk <pashaabramchuk92@gmail.com>
2020-12-02 14:59:42 +0200

    complete

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// 1) custom map
+// 1) map
 
-/* var arr = [1, 2, 3, 4, 5];
+var arr = [1, 2, 3, 4, 5];
 
-function customMap(arr) {
+function toSquare(arr) {
     var result = [];
 
     for (var i = 0; i < arr.length; i++) {
@@ -13,15 +13,15 @@ function customMap(arr) {
     return result;
 }
 
-var res = customMap(arr);
-console.log(res); */
+var res = toSquare(arr);
+console.log(res);
 
 
-// 2) custom filter
+// 2) filter
 
 var arr = [1, 2, 3, 4, 5];
 
-function customFilter(arr) {
+function checkMultiplicity(arr) {
     var result = [];
 
     for (var i = 0; i < arr.length; i++) {
@@ -35,8 +35,44 @@ function customFilter(arr) {
     return result;
 }
 
-var res = customFilter(arr);
+var res = checkMultiplicity(arr);
 
 console.log(res);
 
 // 3) main task
+
+var notification = [
+    {
+        date: '31/07/2019',
+        msg: 'message 1'
+    },
+    {
+        date: '31/07/2019',
+        msg: 'message 2'
+    },
+    {
+        date: '30/07/2019',
+        msg: 'message 3'
+    },
+    {
+        date: '25/09/2019',
+        msg: 'message 4'
+    }
+];
+
+var result = notification.reduce(function(acc, info) {
+
+    if(info.date === info.date) {
+        acc[info.date];
+
+        if(!acc[info.date]) {
+            acc[info.date] = [];
+        }
+        
+        acc[info.date].push(info.msg);
+    }
+    
+    return acc;
+}, {});
+
+console.log(result);
